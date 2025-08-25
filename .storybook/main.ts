a58d6e1793954b3b374e374f8da7fd1d120b8ad1
estuda-neto <clodoaldobritodev@gmail.com>
2025-08-25 15:40:33 -0300

    chore: configured storybook in next project

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+
+  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
+
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  }
+};
+export default config;
