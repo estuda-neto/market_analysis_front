83a2c7fb3a8ed5f9faae5c9212bb4bf682739b08
estuda-neto <clodoaldobritodev@gmail.com>
2025-08-25 23:12:03 -0300

    feat: public and private architecture initiated

---
+++ src/app/(private)/manager/page.tsx
@@ -0,0 +1,7 @@
+export default function Manager() {
+  return (
+    <div className="w-full h-full">
+      Manager
+    </div>
+  );
+}
---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,7 @@
+  export default function Home() {
+  return (
+    <div className="w-full h-full">
+      <h2>home</h2>
+    </div>
+  );
+}
